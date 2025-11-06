81cfb4d9450f3c4ff43d3d8099379e4a7af01011
Khang <khang692004@gmail.com>
2025-11-06 12:42:46 +0700

    file setup

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+//middlewares
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server khởi động ở cổng ${PORT}`);
+})
