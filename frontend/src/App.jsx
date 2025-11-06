702a5b3c3b2b7728d45bb369c747b498dba8883e
Khang <khang692004@gmail.com>
2025-11-06 17:51:39 +0700

    'shadcn+tailwindcss'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
-
-
+import { BrowserRouter, Route, Routes } from 'react-router'
+import StartPage from './pages/StartPage'
+import SignInPage from './pages/SignInPage'
+import { Toaster } from 'sonner'
 function App() {
 
-  return <>Hello World</>
+  return <>
+    <Toaster />
+    <BrowserRouter>
+      <Routes>
+        {/* public route */}
+        <Route
+          path='/'
+          element={<StartPage />}
+        ></Route>
+        <Route
+          path='/signin'
+          element={<SignInPage />}
+        ></Route>
+      </Routes>
+      {/* protected route */}
+    </BrowserRouter>
+
+  </>
 
 }
 
